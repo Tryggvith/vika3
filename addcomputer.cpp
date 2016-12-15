07801b22e23cb043226b5f535f94a2fa5182f9e7
audurara <audurbjorkara@gmail.com>
2016-12-15 14:59:41 +0000

    input validation fyrir add computer

--- addcomputer.cpp
+++ addcomputer.cpp
@@ -25,12 +25,39 @@ void AddComputer::on_Button_Add_Computer_clicked()
     string brand = ui->Input_Computer_Model->text().toStdString();
     string constr = ui->Input_Computer_Yes->text().toStdString();
 
+    bool therewasanError = false;
+
     if(name.empty() || buildy.empty() || brand.empty() || constr.empty())
     {
         ui->label_error->setText("<span style='color: red'>No fields can be empty!</span>");
 
         return;
     }
+
+    int bvalue = atoi(buildy.c_str());
+    int bYearLength = buildy.length();
+
+    for(int i = 0; i < bYearLength; i++)
+    {
+        if(!isdigit(buildy[i]))
+        {
+            ui->label_error_buildy->setText("<span style='color: red'>Invalid input!</span>");
+            bYearLength = buildy.length();
+            therewasanError = true;
+        }
+    }
+        if(bvalue < 0 || bvalue > 2016)
+        {
+            ui->label_error_buildy->setText("<span style='color: red'>Invalid input!</span>");
+
+            bvalue = atoi(buildy.c_str());
+            therewasanError = true;
+        }
+
+        if(therewasanError)
+        {
+            return;
+        }
 
     _service.addComputer(name, buildy, brand, constr);
     this->done(0);
